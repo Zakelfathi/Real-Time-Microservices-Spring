c632db38031761fb0aad94d445c017e7ff2f3bb5
zakelfathi <zakelfathi@gmail.com>
2023-08-15 21:10:34 +0100

    add product api(get/post) && req/resp

--- src/main/java/com/products/productservice/dto/ProductResponse.java
+++ src/main/java/com/products/productservice/dto/ProductResponse.java
@@ -1,2 +1,20 @@
-package com.products.productservice.dto;public class ProductResponse {
+package com.products.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+    private  String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
 }
--- src/main/java/com/products/productservice/repository/ProductRepository.java
+++ src/main/java/com/products/productservice/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.products.productservice.repository;
 
-public interface ProductRepository {
+import com.products.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
 }
